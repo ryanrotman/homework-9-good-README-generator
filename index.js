809dce5c8ed8cd41af91674c292b143e7fb2c2f2
Ryan Rotman <ryan.rotman@gmail.com>
2020-09-22 01:04:52 -0500

    adjusted the list of questions that the user will be prompted with

--- index.js
+++ index.js
@@ -1,52 +1,66 @@
 let inquirer = require("inquirer");
 let fs = require("fs");
+let util = require("util");
 
-inquirer.prompt([
-    {
-        type: "input",
-        message: "What is the title of your project?",
-        name: "title",
-    },
-    {
-        type: "input",
-        message: "What is the description of your project?",
-        name: "description",
-    },
-    {
-        type: "input",
-        message: "What is the table of contents?",
-        name: "tableOfContents",
-    },
-    {
-        type: "input",
-        message: "What are the installation instructions?",
-        name: "installation",
-    },
-    {
-        type: "input",
-        message: "What is the usage of this app?",
-        name: "usage",
-    },
-    {
-        type: "input",
-        message: "What license(s) are being used?",
-        name: "license",
-    },
-    {
-        type: "input",
-        message: "Who is contributing to this project?",
-        name: "contributing",
-    },
-    {
-        type: "input",
-        message: "How is this project tested?",
-        name: "tests",
-    },
-    {
-        type: "input",
-        message: "Any questions?",
-        name: "questions",
-    }
-]).then(function(response) {
-    console.log(response); // This is for testing purposes. Will replace with writefile.
-});
+let writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the title of your project?",
+            name: "title",
+        },
+        {
+            type: "input",
+            message: "What is the description of your project?",
+            name: "description",
+        },
+        {
+            type: "input",
+            message: "What are the installation instructions?",
+            name: "installation",
+        },
+        {
+            type: "input",
+            message: "What is the usage of this app?",
+            name: "usage",
+        },
+        {
+            type: "input",
+            message: "Who is contributing to this project?",
+            name: "contributing",
+        },
+        {
+            type: "input",
+            message: "How is this project tested?",
+            name: "tests",
+        },
+        {
+            type: "list",
+            message: "Select what license is being used?",
+            name: "license",
+            choices: [
+                "Apache-2.0",
+                "BSD-3-Clause",
+                "BSD-2-Clause",
+                "GPL-License",
+                "LGPL-License",
+                "MIT",
+                "MPL-2.0",
+                "CDDL-1.0",
+                "EPL-2.0"
+            ]
+        },
+        {
+            type: "input",
+            message: "What is your GitHub username?",
+            name: "github",
+        },
+        {
+            type: "input",
+            message: "What is your email address?",
+            name: "email",
+        }
+    ]);
+}
