eb94354caa9799309bc808f7dac43e80989897aa
Ryan Rotman <ryan.rotman@gmail.com>
2020-09-23 18:17:25 -0500

    removed white space in the code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,43 +1,43 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
     return `
-    # ${data.title} | ![License Badge](https://img.shields.io/badge/license-${data.license}-green)
+# ${data.title} | ![License Badge](https://img.shields.io/badge/license-${data.license}-green)
 
-    ## Description
-    
-    ${data.description}
-    
-    ## Table of Contents
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [License](#license)
-    * [Contributing](#contributing)
-    * [Tests](#tests)
-    * [Questions](#questions)
-    
-    ## Installation
-    
-    ${data.installation}
-    
-    ## Usage
-    
-    ${data.usage}
-    
-    ## License
-    
-    This project is covered under the ${data.license} license and you can find additional information about that license [here](https://opensource.org/licenses/${data.license}).
-    
-    ## Contributing
-    
-    ${data.contributing}
-    
-    ## Tests
-    
-    ${data.tests}
-    
-    ## Questions
-    
-    For any clarifications or additional questions regarding this project, you can view my [GitHub profile](https://github.com/${data.github}) or contact me by email at ${data.email}.`;
-  }
+## Description
+
+${data.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+
+This project is covered under the ${data.license} license and you can find additional information about that license [here](https://opensource.org/licenses/${data.license}).
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+For any clarifications or additional questions regarding this project, you can view my [GitHub profile](https://github.com/${data.github}) or contact me by email at ${data.email}.`;
+}
   
   module.exports = generateMarkdown;
